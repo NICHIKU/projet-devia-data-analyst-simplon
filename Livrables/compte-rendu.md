6b89173f27d92989576efa61d73eb0971c610224
Ethan Puype <43581889+NICHIKU@users.noreply.github.com>
2025-10-25 16:27:38 +0000

    Soumission du projet technique

---
+++ Livrables/compte-rendu.md
@@ -0,0 +1,27 @@
+# **Puype Ethan**
+
+Bonjour, vous êtes venus vers moi afin de vous rendre une analyse approfondi de vos ventes en prenant en compte 3 paramètres principaux :
+
+- Votre chiffre d'affaires
+- Le nombre de ventes par produit
+- Le nombre de ventes par région
+
+
+
+Avec un chiffres d'affaires de 44825 € au cours de ce dernier mois, il semblerait que vos produits semblent connaitrent un grand succès.
+
+
+
+De plus, vos produits touchent nos régions nord et sud avec un succés quasiment égal, avec le sud qui prends malgré tout une certaine avance qui constitue 52.5% de vos ventes.
+
+
+
+En revanche pour de ce qui concerne la vente de vos produits il y a un gros point noir,
+
+Là où vous avez vendu 1750 de votre produit A et 1055 de votre produit B (ce qui constitue une nette baisse mais cela est rattrapable),
+
+Votre produit C quand à lui n'a été vendu que 575 fois (soit 17% de vos ventes là où le produit B a 31.2% et le produit A a 51.8%)
+
+Soit une baisse de quasiment 45.5% par rapport au produit B et une baisse de 67.1% par rapport au produit A
+
+Je vous conseille donc de vous concentrer sur vos produits A et B et de continuer d'améliorer leur ventes et de cesser la production de votre produit C afin de limiter les coûts de production et augmenter votre bénéfice
--- app.py
+++ app.py
@@ -3,8 +3,37 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
 
-figure.write_html('ventes-par-region.html')
+#question 3A : chiffre d'affaires total
+'''
+données['ca'] = données['prix'] * données['qte']
 
-print('ventes-par-région.html généré avec succès !')
+ca_total_global = données['ca'].sum()
+
+df_total = pd.DataFrame({'Catégorie': ['CA Total'], 'Montant': [ca_total_global]})
+
+figure = px.bar(df_total , x= 'Catégorie', y='Montant', title='Chiffre d\'affaire total', labels={'Montant': 'Chiffre d\'affaire (en €)', 'Catégorie': ''} , text='Montant')
+'''
+
+#Question 3B : ventes par produit
+#figure = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
+
+#Question 3C : ventes par région
+#figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+
+#Question 6A : ventes par produit
+'''
+qte_par_produit = données.groupby('produit')['qte'].sum().reset_index()
+figure = px.bar(qte_par_produit, x='produit', y='qte', title='Quantité totale vendue par produit')
+'''
+
+#Question 6B : chiffre d'affaires par produit
+données['chiffre_affaires'] = données['prix'] * données['qte']
+
+ca_produit = données.groupby('produit')['chiffre_affaires'].sum().reset_index()
+figure = px.bar(ca_produit, x='produit', y='chiffre_affaires', title='Chiffre d\'affaires par produit', labels={'chiffre_affaires': 'Chiffre d\'affaires (en €)', 'produit': 'Produit'}, text='chiffre_affaires')
+
+
+figure.write_html('ventes_produit_barres.html')
+
+print('fichier généré avec succès !')
